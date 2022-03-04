ee9f9249f2bfe0bc88cd190e725efc2d9b6b1a94
shadeemerhi <shadmerhi@gmail.com>
2022-03-04 00:17:19 -0700

    increase navbar size

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -4,7 +4,7 @@ import styles from "../../styles/Layout.module.scss";
 
 const Navbar: React.FC = () => {
   return (
-    <div className={`${styles.navbar} heavy_text logo_text md_text`}>
+    <div className={`${styles.navbar} heavy_text logo_text lg_text`}>
       Intellicents
     </div>
   );
