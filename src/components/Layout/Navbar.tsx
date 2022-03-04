7e360ae55d7c26f06937533d10adfd763ab2155a
shadeemerhi <shadmerhi@gmail.com>
2022-03-04 00:15:32 -0700

    enhance navbar

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
+import styles from "../../styles/Layout.module.scss";
+
 const Navbar: React.FC = () => {
-  return <div>Here is the Navbar</div>;
+  return (
+    <div className={`${styles.navbar} heavy_text logo_text md_text`}>
+      Intellicents
+    </div>
+  );
 };
 export default Navbar;
